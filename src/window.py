ca61473b7c4695731881119c43e13f57afa17f08
Herpiko Dwi Aguno <herpiko@gmail.com>
2023-09-23 03:27:13 +0700

    Working PoC.

--- src/window.py
+++ src/window.py
@@ -26,8 +26,10 @@
 # use or other dealings in this Software without prior written
 # authorization.
 
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk, GLib
 import random
+import time
+import threading
 
 
 @Gtk.Template(resource_path='/xyz/aguno/CubeTimer/window.ui')
@@ -36,29 +38,91 @@ class CubetimerWindow(Gtk.ApplicationWindow):
 
     ScrambleNotation = Gtk.Template.Child("ScrambleNotation")
     Timer = Gtk.Template.Child("Timer")
-    
+    Menu = Gtk.Template.Child("Menu")
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        scrambled = self.generate_scramble()
-        print(scrambled) 
-        self.ScrambleNotation.set_text(scrambled)
-        print("helloooo")
-       
-        # Events 
-        #self.Timer.connect("key-press-event", self.start_timer)
         
+        # Events 
+        event_controller = Gtk.EventControllerKey()
+        event_controller.connect("key-pressed", self.on_key_pressed)
+        event_controller.connect("key-released", self.on_key_released)
+        self.add_controller(event_controller)
+        
+                         
+        self.timer_running = False
+        self.start_time = None
+        self.elapsed_time = 0
+        self.reset_id = 0
+        self.state = "reset"
+        
+        self.Menu.set_can_focus(False)
+        self.Timer.set_can_focus(True)
+        self.Timer.grab_focus()
+        
+        self.generate_scramble()
+       
     def generate_scramble(self):
         moves = ["U", "U'", "U2", "D", "D'", "D2", "L", "L'", "L2", "R", "R'", "R2", "F", "F'", "F2", "B", "B'", "B2"]
         scramble = []
         for _ in range(21):
             random_move = random.choice(moves)
             scramble.append(random_move)
-        return "  ".join(scramble)
+        scrambled =  "  ".join(scramble)
+        self.ScrambleNotation.set_text(scrambled)
+
+    def update_timer(self):
+        print("yo1")
+        while self.timer_running:
+            GLib.idle_add(self.update_label)
+            time.sleep(0.001)           
+            
+    def update_label(self):
+        self.elapsed_time = time.time() - self.start_time
+        minutes = int(self.elapsed_time // 60)
+        seconds = int(self.elapsed_time % 60)
+        milliseconds = int((self.elapsed_time % 1) * 1000)
+        self.Timer.set_label(f"{minutes:02}:{seconds:02}.{milliseconds:03}")
+       
+    def on_start_timer(self):
+        self.timer_running = True
+        self.start_time = time.time()
+        self.thread = threading.Thread(target=self.update_timer)
+        self.thread.start()
         
-    def start_timer(self):
-        print("ok")
+    def on_key_released(self, controller, keyval, keycode, state):
+        if keyval == Gdk.KEY_space:
+            if not self.timer_running and self.elapsed_time == 0 and self.state == "ready":
+                self.state = "running"
+                self.timer_running = True
+                self.start_time = time.time()
+                self.thread = threading.Thread(target=self.update_timer)
+                self.thread.start()
+                
+    def on_key_pressed(self, controller, keyval, keycode, state):
+        if keyval == Gdk.KEY_space:
+            if self.state == "reset":
+                GLib.timeout_add(100, self.ready)
+            if self.state == "stopped":
+                self.reset()
+            if self.timer_running:
+                self.state = "stopped"
+                self.timer_running = False
+                self.generate_scramble()
+                
+    def ready(self):
+        self.state = "ready"
         
+    def reset(self):
+        if self.state == "stopped":
+            self.elapsed_time = 0
+            minutes = int(self.elapsed_time // 60)
+            seconds = int(self.elapsed_time % 60)
+            milliseconds = int((self.elapsed_time % 1) * 1000)
+            self.Timer.set_label(f"{minutes:02}:{seconds:02}.{milliseconds:03}")
+            self.state = "ready"
+
+       
 
 
 class AboutDialog(Gtk.AboutDialog):
