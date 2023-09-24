12d76e81c780c71b4c8becb6bd618ceafc80b159
Herpiko Dwi Aguno <herpiko@gmail.com>
2023-09-24 16:01:46 +0700

    Use string instead of byte.

--- src/window.py
+++ src/window.py
@@ -69,7 +69,7 @@ class CubetimerWindow(Gtk.ApplicationWindow):
 
     def load_css(self):
         css_provider = Gtk.CssProvider()
-        css_provider.load_from_data(b"""
+        css_provider.load_from_data("""
             .scramble-label {
                 font-size: 18px; /* Adjust the size as needed */
                 color: grey;
@@ -81,7 +81,7 @@ class CubetimerWindow(Gtk.ApplicationWindow):
     def set_timer_color(self, color):
         css_provider = Gtk.CssProvider()
         if color == "grey":
-            css_provider.load_from_data(b"""
+            css_provider.load_from_data("""
             .timer-label {
                 font-size: 60px; /* Adjust the size as needed */
                 color: grey;
@@ -89,14 +89,14 @@ class CubetimerWindow(Gtk.ApplicationWindow):
             }
             """, "utf-8")
         if color == "green":
-            css_provider.load_from_data(b"""
+            css_provider.load_from_data("""
             .timer-label {
                 font-size: 60px; /* Adjust the size as needed */
                 color: green;
             }
             """, "utf-8")
         if color == "orange":
-            css_provider.load_from_data(b"""
+            css_provider.load_from_data("""
             .timer-label {
                 font-size: 60px; /* Adjust the size as needed */
                 color: orange;
@@ -104,7 +104,7 @@ class CubetimerWindow(Gtk.ApplicationWindow):
             """, "utf-8")
 
         if color == "red":
-            css_provider.load_from_data(b"""
+            css_provider.load_from_data("""
             .timer-label {
                 font-size: 60px; /* Adjust the size as needed */
                 color: red;
